ff703c65924880911ce5ab76414bfac8c64561e7
KG8905 <167706923+KG8905@users.noreply.github.com>
2024-09-25 21:29:48 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +10,14 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        secure: false, // Can be true in production
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optionally rewrite the path
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // Customize output directory
+    sourcemap: true, // Enable source maps for easier debugging
+  },
+});
